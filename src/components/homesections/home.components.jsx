cbb7cc285a12fa170b69b6233f3f73a0f8e57f57
unknown <wanzerox@gmail.com>
2019-12-14 17:03:17 +0700

    add Home section

---
+++ src/components/homesections/home.components.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './home.styles.scss'
+
+
+const Homesection = () => (
+    <div className='home-section'>
+        <h1>Hi, I'm Wantanawat Jitprakop</h1>
+        <span>looking for job</span>
+    </div>
+)
+
+export default Homesection
