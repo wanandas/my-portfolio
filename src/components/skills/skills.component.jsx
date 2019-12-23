c999cc6ce81683914995149a80f94d7ed5be97e6
unknown <wanzerox@gmail.com>
2019-12-23 23:58:01 +0700

    @media and build skiils section

--- src/components/skills/skills.component.jsx
+++ src/components/skills/skills.component.jsx
@@ -5,8 +5,35 @@ import "./skills.styles.scss";
 const SkillsSection = () => (
   <div className="skills-section">
     <hr></hr>
-    <h2 className="title">Skills</h2>
-    <div className="skills-container"></div>
+    <h2 className="title">My Skill</h2>
+    <div className="skills-container">
+      <div className="skills">
+        <div className="skill">
+          <h3 className="skill_type">HTML</h3>
+          <div className="skill__ouuter-bar">
+            <div className="skill__inner-bar skill__inner-bar--50"></div>
+          </div>
+        </div>
+        <div className="skill">
+          <h3 className="skill_type">CSS</h3>
+          <div className="skill__ouuter-bar">
+            <div className="skill__inner-bar skill__inner-bar--45"></div>
+          </div>
+        </div>
+        <div className="skill">
+          <h3 className="skill_type">JS</h3>
+          <div className="skill__ouuter-bar">
+            <div className="skill__inner-bar skill__inner-bar--40"></div>
+          </div>
+        </div>
+        <div className="skill">
+          <h3 className="skill_type">REACT&REDUX</h3>
+          <div className="skill__ouuter-bar">
+            <div className="skill__inner-bar skill__inner-bar--30"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
