2d376c70c102f50b34fad2eda7add4b3f7f0d5e5
unknown <wanzerox@gmail.com>
2019-12-30 17:41:11 +0700

    fixed

--- src/components/homesections/home.component.jsx
+++ src/components/homesections/home.component.jsx
@@ -4,7 +4,6 @@ import { TimelineMax } from "gsap";
 import "./home.styles.scss";
 import ScrollMagic from "scrollmagic";
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
-
 ScrollMagicPluginGsap(ScrollMagic, TimelineMax);
 
 class Homesection extends React.Component {
@@ -13,25 +12,39 @@ class Homesection extends React.Component {
     this.controller = new ScrollMagic.Controller();
 
     this.tl = new TimelineMax();
-    this.head = null;
+    this.header = null;
     this.span = null;
   }
 
   componentDidMount() {
-    this.tl.from(this.head, 1, { x: -200, opacity: 0 });
-    this.tl.from(this.span, 1, { x: 200, opacity: 0 }, "=-.5");
-
     const scene = new ScrollMagic.Scene({
-      triggerElement: "#head"
+      triggerElement: "#header"
     })
       .setTween(this.tl)
       .addTo(this.controller);
+
+    this.tl.fromTo(
+      this.header,
+      1,
+      { x: -200, opacity: 0 },
+      { x: 0, opacity: 1 }
+    );
+
+    this.tl.fromTo(
+      this.span,
+      1,
+      { x: 200, opacity: 0 },
+      { x: 0, opacity: 1 },
+      "=-.7"
+    );
   }
 
   render() {
     return (
       <div className="home-section" name="headerSection">
-        <h1 ref={h1 => (this.head = h1)}>Hi, I'm Wantanawat Jitprakop</h1>
+        <h1 ref={h1 => (this.header = h1)} id="header">
+          Hi, I'm Wantanawat Jitprakop
+        </h1>
         <span ref={span => (this.span = span)}>looking for job</span>
       </div>
     );
